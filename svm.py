cea23abd46b3e4d66840e82af058e55e87896463
Michal Gandor <gandor.michal@gmail.com>
2018-05-06 18:02:31 +0200

    bump

--- svm.py
+++ svm.py
@@ -24,12 +24,12 @@ y = np.array(df['PM'])
 windowSize = 144
 
 #X_train2, X_test2, y_train2, y_test2 = train_test_split(X, y, test_size=0.2)
-i = 0
+i = windowSize
 
-X_test = np.array([[1,1,1]])
-y_test = np.array([1])
+X_test = np.array([[0,0,0]])
+y_test = np.array([0])
 
-while i < len(X) - windowSize:
+while i < len(df) - windowSize:
     #create test date variables
     idx = list(range(i, i + 24))
     X_test = np.append(X_test, X[idx], axis=0)
@@ -38,10 +38,10 @@ while i < len(X) - windowSize:
 
 i = 0
 
-newX = np.array([[1, 1, 1]])
-newY = np.array([1])
+newX = np.array([[0,0,0]])
+newY = np.array([0])
 
-while i < len(X) - windowSize:
+while i < len(df) - windowSize:
     #create test date variables
     idx = list(range(i, i + windowSize))
     newX = np.append(newX, X[idx], axis=0)
@@ -54,8 +54,8 @@ y = newY
 
 for z in range(1, 20):
 
-    clf = svm.SVC(kernel='linear', gamma=z * 10, C=z)
-    i = 2;
+    clf = svm.SVC(kernel='linear')
+    i = 0;
     while i < len(X) - windowSize:
         idx = list(range(i, i + windowSize))
         i = i + windowSize;
@@ -72,7 +72,7 @@ for z in range(1, 20):
 for z in range(1, 20):
 
     clf = svm.SVC(kernel='rbf', gamma=0.01, C=100)
-    i = 2;
+    i = 0;
     while i < len(X) - windowSize:
         idx = list(range(i, i + windowSize))
         i = i + windowSize;
@@ -85,8 +85,6 @@ for z in range(1, 20):
     print("Gamma: ", z/1000, " C: ", z / 100)
     print("Confidence: ", confidence)
 
-d = clf.predict(X[3:6]);
-print(d)
 
 forecast_col = 'PM'
 df.fillna(value=-99999, inplace=True)
